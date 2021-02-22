10220f0605b3fca68c5382949a9778843f1d606b
mkln <peruzzi@gmail.com>
2021-02-22 19:00:11 +0100

    omp fix

--- src/spamtree_fit.cpp
+++ src/spamtree_fit.cpp
@@ -55,7 +55,11 @@ Rcpp::List spamtree_mv_mcmc(
     bool sample_w=true,
     bool sample_predicts=true){
   
+  
+#ifdef _OPENMP
   omp_set_num_threads(num_threads);
+#endif
+  
   std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
   std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
   
